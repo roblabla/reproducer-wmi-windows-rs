2f3a6d18651d6a4be967220ded66a94f0c38fd04
Vincent Thiberville <vthib@pm.me>
2021-09-22 17:20:58 +0200

    update minimal reproducer for segfault
    Running this binary triggers a segfault more than 50% of the time
    on a W10 VM.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
 fn main() {
-    //let handle1 = std::thread::spawn(thread1);
+    let handle1 = std::thread::spawn(thread1);
     let handle2 = std::thread::spawn(thread2);
-    //handle1.join().unwrap();
+    handle1.join().unwrap();
     handle2.join().unwrap();
 }
 
 windows::include_bindings!();
 
-fn thread1() {
-    loop {
-        //println!("Thread1 loop");
-        let com_con = wmi::COMLibrary::without_security().unwrap();
-        let wmi_con = wmi::WMIConnection::new(com_con.into()).unwrap();
-        let results = wmi_con
-            .exec_query_native_wrapper("SELECT OSArchitecture FROM Win32_OperatingSystem").unwrap()
-            .collect::<Vec<_>>();
+fn inner_wmi() {
+    let com_con = wmi::COMLibrary::without_security().unwrap();
+    let wmi_con = wmi::WMIConnection::new(com_con.into()).unwrap();
+    let results = wmi_con
+        .exec_query_native_wrapper("SELECT OSArchitecture FROM Win32_OperatingSystem").unwrap()
+        .collect::<Vec<_>>();
+}
+
+fn inner_wrt() {
+    let before = std::time::Instant::now();
+    let pm = Windows::Management::Deployment::PackageManager::new().ok().unwrap();
+    let iter = pm.FindPackages().ok().unwrap();
+    for package in iter {
+        let path = package.InstalledLocation().and_then(|v| v.Path());
+        //println!("{:?}", path);
     }
+    let after = std::time::Instant::now();
+    println!("Time: {:?}", after - before);
+}
+
+fn thread1() {
+    inner_wmi();
+    inner_wrt();
 }
 
 fn thread2() {
-    loop {
-        let before = std::time::Instant::now();
-        let pm = Windows::Management::Deployment::PackageManager::new().ok().unwrap();
-        let iter = pm.FindPackages().ok().unwrap();
-        for package in iter {
-            let path = package.InstalledLocation().and_then(|v| v.Path());
-            //println!("{:?}", path);
-        }
-        let after = std::time::Instant::now();
-        println!("Time: {:?}", after - before);
-    }
+    inner_wmi();
 }
